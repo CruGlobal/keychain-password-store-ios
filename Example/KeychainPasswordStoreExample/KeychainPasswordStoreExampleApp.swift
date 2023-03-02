2361360c18cfdc03162cd08363b61ef76516fcbc
Levi Eggert <levi.eggert@cru.org>
2023-03-02 09:21:04 -0500

    Create example project with tests for testing against keychain

---
+++ Example/KeychainPasswordStoreExample/KeychainPasswordStoreExampleApp.swift
@@ -0,0 +1,20 @@
+//
+//  KeychainPasswordStoreExampleApp.swift
+//  KeychainPasswordStoreExample
+//
+//  Created by Levi Eggert on 3/1/23.
+//
+
+import SwiftUI
+
+@main
+struct KeychainPasswordStoreExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            VStack(alignment: .leading, spacing: 0) {
+                Text("Keychain Password Store")
+                    .foregroundColor(Color.black)
+            }
+        }
+    }
+}
